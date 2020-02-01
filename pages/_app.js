3fc24f216e0848a7dc596f06b30614a3930ea8be
chriswynn <chris@wynncodes.com>
2020-02-01 13:39:55 -0500

    refactor: set background to black

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import "minireset.css";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div css={{ background: "#000" }}>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
 export default App;
