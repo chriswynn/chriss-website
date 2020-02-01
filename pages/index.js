df9f2b17a903ccf3d00b6ed5e9ca2529dcd85c93
chriswynn <chris@wynncodes.com>
2020-02-01 13:33:01 -0500

    feat: add photos homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+
 function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div
+      css={{
+        maxWidth: "75%",
+        margin: "0 auto",
+        "& img": {
+          marginBottom: "5%"
+        }
+      }}
+    >
+      <img src="/img/photos/P1120481.JPG" />
+      <img src="/img/photos/P1120484.JPG" />
+      <img src="/img/photos/P1120487.JPG" />
+      <img src="/img/photos/P1120488.JPG" />
+      <img src="/img/photos/P1120489.JPG" />
+      <img src="/img/photos/P1120492.JPG" />
+      <img src="/img/photos/P1120494.JPG" />
+      <img src="/img/photos/P1120499.JPG" />
+      <img src="/img/photos/P1120501.JPG" />
+      <img src="/img/photos/P1120505.JPG" />
+      <img src="/img/photos/P1120506.JPG" />
+      <img src="/img/photos/P1120508.JPG" />
+      <img src="/img/photos/P1120509.JPG" />
+      <img src="/img/photos/P1120511.JPG" />
+      <img src="/img/photos/P1120513.JPG" />
+      <img src="/img/photos/P1120514.JPG" />
+    </div>
+  );
 }
 
 export default Home;
