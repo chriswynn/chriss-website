5610eddc0b16da2e091f5c25456f46266f0b081a
chriswynn <chris@wynncodes.com>
2020-03-11 08:22:28 -0400

    refactor: add ratio box to images

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,48 @@
 function Home() {
   return (
     <div className="photos">
-      <img src="/img/photos/P1120481.JPG" />
-      <img src="/img/photos/P1120484.JPG" />
-      <img src="/img/photos/P1120487.JPG" />
-      <img src="/img/photos/P1120488.JPG" />
-      <img src="/img/photos/P1120489.JPG" />
-      <img src="/img/photos/P1120492.JPG" />
-      <img src="/img/photos/P1120494.JPG" />
-      <img src="/img/photos/P1120499.JPG" />
-      <img src="/img/photos/P1120501.JPG" />
-      <img src="/img/photos/P1120505.JPG" />
-      <img src="/img/photos/P1120506.JPG" />
-      <img src="/img/photos/P1120508.JPG" />
-      <img src="/img/photos/P1120509.JPG" />
-      <img src="/img/photos/P1120511.JPG" />
-      <img src="/img/photos/P1120513.JPG" />
-      <img src="/img/photos/P1120514.JPG" />
+      <div className="ratio-box">
+        <img src="/img/photos/P1120487.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120488.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120489.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120492.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120494.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120499.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120501.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120505.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120506.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120508.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120509.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120511.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120513.JPG" />
+      </div>
+      <div className="ratio-box">
+        <img src="/img/photos/P1120514.JPG" />
+      </div>
     </div>
   );
 }
