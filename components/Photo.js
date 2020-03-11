476835fc2cca0ca80a9462d56b9205f708bec290
chriswynn <chris@wynncodes.com>
2020-03-11 08:31:58 -0400

    feat: add Photo component

---
+++ components/Photo.js
@@ -0,0 +1,7 @@
+export default function(props) {
+  return (
+    <div className="ratio-box">
+      <img src={props.src} />
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,22 @@
+import Photo from "../components/Photo";
+
 function Home() {
   return (
     <div className="photos">
-      <div className="ratio-box">
-        <img src="/img/photos/P1120487.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120488.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120489.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120492.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120494.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120499.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120501.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120505.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120506.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120508.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120509.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120511.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120513.JPG" />
-      </div>
-      <div className="ratio-box">
-        <img src="/img/photos/P1120514.JPG" />
-      </div>
+      <Photo src="/img/photos/P1120487.JPG" />
+      <Photo src="/img/photos/P1120488.JPG" />
+      <Photo src="/img/photos/P1120489.JPG" />
+      <Photo src="/img/photos/P1120492.JPG" />
+      <Photo src="/img/photos/P1120494.JPG" />
+      <Photo src="/img/photos/P1120499.JPG" />
+      <Photo src="/img/photos/P1120501.JPG" />
+      <Photo src="/img/photos/P1120505.JPG" />
+      <Photo src="/img/photos/P1120506.JPG" />
+      <Photo src="/img/photos/P1120508.JPG" />
+      <Photo src="/img/photos/P1120509.JPG" />
+      <Photo src="/img/photos/P1120511.JPG" />
+      <Photo src="/img/photos/P1120513.JPG" />
+      <Photo src="/img/photos/P1120514.JPG" />
     </div>
   );
 }
