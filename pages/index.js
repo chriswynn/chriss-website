a598b2b586e5e1fec4eacb221dbe000ce01bcbdd
chriswynn <chris@wynncodes.com>
2020-06-19 22:29:20 -0400

    refactor: adjust images

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,17 @@ import Photo from "../components/Photo";
 function Home() {
   return (
     <div className="photos">
-      <Photo src="/img/photos/P1120487.JPG" />
+      <Photo src="/img/photos/P1120499.JPG" />
       <Photo src="/img/photos/P1120488.JPG" />
       <Photo src="/img/photos/P1120489.JPG" />
       <Photo src="/img/photos/P1120492.JPG" />
       <Photo src="/img/photos/P1120494.JPG" />
-      <Photo src="/img/photos/P1120499.JPG" />
       <Photo src="/img/photos/P1120501.JPG" />
       <Photo src="/img/photos/P1120505.JPG" />
       <Photo src="/img/photos/P1120506.JPG" />
       <Photo src="/img/photos/P1120508.JPG" />
       <Photo src="/img/photos/P1120509.JPG" />
       <Photo src="/img/photos/P1120511.JPG" />
-      <Photo src="/img/photos/P1120513.JPG" />
       <Photo src="/img/photos/P1120514.JPG" />
     </div>
   );
